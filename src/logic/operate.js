e68a0fe6ec2bba868827ebcd6b9ad852c81b7340
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-08 00:24:20 +0200

    Add operations

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,5 +1,35 @@
+import Big from 'big.js';
+
 const operate = (numberOne, numberTwo, operation) => {
-  
+  const nbrOne = Big(numberOne);
+  const nbrTwo = Big(numberTwo);
+  let result = 0;
+
+  switch (operation) {
+    case '+':
+      result = nbrOne.plus(nbrTwo);
+      break;
+
+    case '-':
+      result = nbrOne.minus(nbrTwo);
+      break;
+
+    case 'x':
+      result = nbrOne.times(nbrTwo);
+      break;
+
+    case '%':
+      result = nbrOne.mod(nbrTwo);
+      break;
+
+    case '/':
+      result = nbrOne.div(nbrTwo);
+      break;
+
+    default:
+      break;
+  }
+  return result;
 };
 
 export default operate;
