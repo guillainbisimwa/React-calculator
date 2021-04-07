6a530b616a09131216485d268fcd762d990c2c7c
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-07 14:56:09 +0200

    Fix eslint issues

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Calculator extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
   render() {
     return (
       <div>Calculator</div>
