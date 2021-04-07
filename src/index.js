0e4f85be67b2a63400e4a460849f2a5b426a88bb
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-07 12:04:30 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div />         
+    );
+  }
+}
+
+
+  // ========================================
+  
+  ReactDOM.render(
+    <Calculator />,
+    document.getElementById('root')
+  );
+  
