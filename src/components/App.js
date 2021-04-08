6d46fa147035d772311b003d5e4a37afbda2f7d5
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-08 17:57:51 +0200

    Initialize App states

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
-import calculate from '../logic/calculate';
 
-function App() {
-  calculate();
-  return (
-    <>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: '0',
+      next: '',
+      operation: '',
+    };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <>
         <Display />
         <ButtonPanel />
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
