fa61bafbf674ff6c54da1be854b24113ae04a8a3
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-10 00:29:28 +0200

    Refractor all classes

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './index.css';
 
-class Calculator extends React.Component {
-  // eslint-disable-next-line class-methods-use-this
-  render() {
-    return (
-      <App />
-    );
-  }
-}
+const Calculator = () => (
+    <App />
+);
 
 // ========================================
 
