c80f86a68cc4e632e0dabd0085c6095f500b1658
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-11 18:38:09 +0200

    Use wide prop to indicate that the button should have twice the standard width

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name, color, clickHandler }) => {
+const Button = ({
+  name, color, clickHandler, wide,
+}) => {
   const handleClick = (name) => {
     clickHandler(name);
   };
 
-  const colorBtn = () => `${color}`;
+  const colorBtn = (wideBool) => {
+    if (wideBool) {
+      return `${color} button-equal`;
+    }
+    return `${color}`;
+  };
 
-  return <div className={colorBtn()} onClick={() => handleClick(name)}><span className="text">{name}</span></div>;
+  return <div className={colorBtn(wide)} onClick={() => handleClick(name)}><span className="text">{name}</span></div>;
 };
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
   color: PropTypes.string,
+  wide: PropTypes.bool,
 };
 
 Button.defaultProps = {
   color: 'button btn-primary',
+  wide: false,
 };
 
 export default Button;
