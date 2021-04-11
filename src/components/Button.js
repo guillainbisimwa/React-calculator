db9451254853f52e99508624cf2547a3af30dc03
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-11 18:29:34 +0200

    Use the color prop to customize the color of the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name, clickHandler }) => {
+const Button = ({ name, color, clickHandler }) => {
   const handleClick = (name) => {
     clickHandler(name);
   };
 
-  const isNumber = (n) => /^-?[\d.]+(?:e-?\d+)?$/.test(n);
-  if (name === '0') {
-    return <div className="button button-equal" onClick={() => handleClick(name)}><span className="text">{name}</span></div>;
-  }
-  if (isNumber(name)) {
-    return <div className="button" onClick={() => handleClick(name)}><span className="text">{name}</span></div>;
-  }
-  return <div className="button btn-primary" onClick={() => handleClick(name)}><span className="text">{name}</span></div>;
+  const colorBtn = () => `${color}`;
+
+  return <div className={colorBtn()} onClick={() => handleClick(name)}><span className="text">{name}</span></div>;
 };
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
   clickHandler: PropTypes.func.isRequired,
+  color: PropTypes.string,
+};
+
+Button.defaultProps = {
+  color: 'button btn-primary',
 };
 
 export default Button;
