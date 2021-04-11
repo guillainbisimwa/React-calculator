37e342befe0634a0644fc18cf07f17d27edf33dc
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-11 20:11:44 +0200

    Create Header Component

---
+++ src/screens/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { Box } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <div width={1} className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+          Math Magicians
+          </Typography>
+          <Box justifyContent="flex-end">
+            <Button color="inherit">Header</Button>
+            <Button color="inherit">Calculator</Button>
+            <Button color="inherit">Quote</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default Header;
