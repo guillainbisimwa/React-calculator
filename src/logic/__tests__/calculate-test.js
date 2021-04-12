ee72ec80a011cf69dc216d8becf5f6c09405ea91
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-04-13 00:02:25 +0200

    Test if a AC button is clicked

--- src/logic/__tests__/calculate-test.js
+++ src/logic/__tests__/calculate-test.js
@@ -10,4 +10,10 @@ describe('all calculator buttons works correctly', () => {
     expect(calculate(data, '7').next).toBe(null);
     expect(calculate(data, '6').next).not.toBe('3');
   });
+
+  it('AC resets calculator state', () => {
+    const data = { total: '3', operation: '+', next: '3' };
+    expect(calculate(data, 'AC')).toEqual({ total: '0', next: null, operation: null });
+    expect(calculate(data, 'AC')).not.toEqual({ total: 0, next: null, operation: null });
+  });
 });
